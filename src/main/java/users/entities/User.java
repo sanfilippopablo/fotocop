3aacea666af21a0d4c1abdb79ea3a55836aeeead
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2015-12-29 17:41:32 -0300

    Created basic User class.

---
+++ src/main/java/users/entities/User.java
@@ -0,0 +1,25 @@
+package users.entities;
+
+/**
+ * 
+ * Representa un User.
+ *
+ */
+public class User {
+	String username;
+	String email;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
