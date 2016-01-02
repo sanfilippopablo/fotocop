8d995e40390164c3f8f80d78503da1f8d4c84542
Pablo Sanfilippo <sanfilippopablo@gmail.com>
2016-01-02 12:19:57 -0300

    Creado el servlet LogoutServlet.

---
+++ src/main/java/users/servlets/LogoutServlet.java
@@ -0,0 +1,34 @@
+package users.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * El servlet logout hace lo mismo tanto por GET
+ * como por POST: Desloguea al usuario seteando
+ * `isLogged` en false y quitando el attr user.
+ */
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LogoutServlet() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().setAttribute("isLogged", false);
+		request.getSession().removeAttribute("user");
+		
+		response.sendRedirect("/");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
