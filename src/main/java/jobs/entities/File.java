620fa2fdb78fb9f7eeb27a2c88f19634f4ca1302
Gonzalo Perez Fariña <chaloperfar@gmail.com>
2016-01-08 17:34:43 -0300

    Agregadas las entidades File y JobLine en Jobs

---
+++ src/main/java/jobs/entities/File.java
@@ -0,0 +1,32 @@
+package jobs.entities;
+
+/**
+ * 
+ * Representa un File / Archivo.
+ *
+ */
+
+public class File {
+	int id;
+	String name;
+	String path;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
---
+++ src/main/java/jobs/entities/JobLine.java
@@ -0,0 +1,48 @@
+package jobs.entities;
+
+/**
+ * 
+ * Representa una Job line / línea de trabajo, correspondiente a un archivo e información sobre qué hay que hacerle.
+ *
+ */
+
+public class JobLine {
+	int quantity;
+	File file;
+	boolean anillado;
+	boolean abrochado;
+	boolean dobleFaz;
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public boolean isAnillado() {
+		return anillado;
+	}
+	public void setAnillado(boolean anillado) {
+		this.anillado = anillado;
+	}
+	public boolean isAbrochado() {
+		return abrochado;
+	}
+	public void setAbrochado(boolean abrochado) {
+		this.abrochado = abrochado;
+	}
+	public boolean isDobleFaz() {
+		return dobleFaz;
+	}
+	public void setDobleFaz(boolean dobleFaz) {
+		this.dobleFaz = dobleFaz;
+	}
+	public File getFile() {
+		return file;
+	}
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+
+}
