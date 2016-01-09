683144c322d2ebe47637ae32f04d9b5b7dd3393e
Gonzalo Perez Fariña <chaloperfar@gmail.com>
2016-01-09 02:15:47 -0300

    Agregada la clase Job en jobs.entities

--- src/main/java/jobs/entities/Job.java
+++ src/main/java/jobs/entities/Job.java
@@ -1,5 +1,72 @@
 package jobs.entities;
+import java.util.ArrayList;
+import java.util.Date;
+
+import users.entities.User;
 
 public class Job {
+	int id;
+	User user;
+	String status;
+	Date creation;
+	Date lastModified;
+	ArrayList<JobLine> jobLines;
+	Date eta;
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getCreation() {
+		return creation;
+	}
+
+	public void setCreation(Date creation) {
+		this.creation = creation;
+	}
+
+	public Date getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(Date lastModified) {
+		this.lastModified = lastModified;
+	}
+
+	public void setJobLines(ArrayList<JobLine> jobLines) {
+		this.jobLines = jobLines;
+	}	
+	
+	public ArrayList<JobLine> getJobLines(){
+
+		return jobLines;
+	}
+
+	public Date getEta() {
+		return eta;
+	}
+
+	public void setEta(Date eta) {
+		this.eta = eta;
+	}
+	
 }
