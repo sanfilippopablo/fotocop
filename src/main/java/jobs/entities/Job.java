194dfb60cd4cddb2a38b72aac081de4f6c5963b0
Gonzalo Perez Fariña <chaloperfar@gmail.com>
2016-01-12 19:17:18 -0300

    Agregado el método getPrintingTime a Job, que calcula y devuelve el tiempo que se tarda en imprimir el trabajo (en milisegundos)

--- src/main/java/jobs/entities/Job.java
+++ src/main/java/jobs/entities/Job.java
@@ -1,4 +1,5 @@
 package jobs.entities;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -19,57 +20,57 @@ public class Job {
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public User getUser() {
 		return user;
 	}
-
 	public void setUser(User user) {
 		this.user = user;
 	}
-
 	public String getStatus() {
 		return status;
 	}
-
 	public void setStatus(String status) {
 		this.status = status;
 	}
-
 	public Date getCreationDate() {
 		return creationDate;
 	}
-
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
 	}
-
 	public Date getLastModifiedDate() {
 		return lastModifiedDate;
 	}
-
 	public void setLastModifiedDate(Date lastModifiedDate) {
 		this.lastModifiedDate = lastModifiedDate;
 	}
-
 	public void setJobLines(ArrayList<JobLine> jobLines) {
 		this.jobLines = jobLines;
 	}	
-	
 	public ArrayList<JobLine> getJobLines(){
 
 		return jobLines;
 	}
-
 	public void addJobLine(JobLine jl){
 		this.jobLines.add(jl);
 	}
 	public Date getEta() {
 		return eta;
 	}
-
 	public void setEta(Date eta) {
 		this.eta = eta;
 	}
-	
+	/**
+	 * Calcula y devuelve el tiempo que se tarda en imprimir el trabajo (en milisegundos).
+	 * 
+	 * 
+	 */ 
+	public long getPrintingTime(){
+		long averagePrintingTimePerFile = 60000; //milisegundos, es decir, 1 minuto
+		long printingT=0;
+		for (JobLine jl : jobLines) {
+			printingT+= averagePrintingTimePerFile * jl.getQuantity();
+		}
+		return printingT;
+	}
 }
